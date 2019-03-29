61efde2bc54263843fd76af56c2394be4188c1bb
1005154606 <47458015+1005154606@users.noreply.github.com>
2019-03-29 13:25:09 -0500

    Tercer commit

--- Potencia.py
+++ Potencia.py
@@ -11,3 +11,10 @@ x=int(input("Introduce la base: "))
 y=int(input("Introduce el exponente: "))
 a_power_b(x,y)
 
+while x>0:
+    x = int(input("Introduce la base: "))
+    if x==0:
+        break
+    y = int(input("Introduce el exponente: "))
+    a_power_b(x, y)
+
