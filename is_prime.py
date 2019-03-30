7aca422b8595849b74a5fb31a118547da871552e
1005154606 <47458015+1005154606@users.noreply.github.com>
2019-03-30 17:20:24 -0500

    Tercer commit

--- is_prime.py
+++ is_prime.py
@@ -1,3 +1,4 @@
+
 def is_prime(a):
 
     c1 = 0
@@ -7,16 +8,30 @@ def is_prime(a):
     if c1 == 2:
         a=1
         print(a)
+
         return a
     else:
         a=0
         print(a)
         return a
 n=1
-
+cont=0
 while n>0:
     try:
         n = int(input("Introduce el número que quieres evaluar: "))
-        is_prime(n)
+        res=is_prime(n)
+        if res==1:
+            cont=cont+1
+
+
     except ValueError:
         print(-1)
+
+print("La cantidad de primos fue",cont)
+n=cont
+
+op=is_prime(n)
+if op==1:
+    print("La cantidad de números primos es primo")
+else:
+    print("La cantidad de números primos no es primo")
