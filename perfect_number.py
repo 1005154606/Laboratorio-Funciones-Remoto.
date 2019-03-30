f02509b157c87025eecf3d87984523d7f6416215
1005154606 <47458015+1005154606@users.noreply.github.com>
2019-03-30 13:55:52 -0500

    segundo commit

--- perfect_number.py
+++ perfect_number.py
@@ -3,8 +3,10 @@ acd=0
 for x in range(1,n):
     if n%x==0:
         acd=acd+x
-if acd==n:
-    print("El número es perfecto")
+resta=acd-n
+print(resta)
+if resta<=3 and resta>=-3:
+    print("El número es casi perfecto")
 else:
-    print("El número no es perfecto")
+    print("El número no es casi perfecto")
 
